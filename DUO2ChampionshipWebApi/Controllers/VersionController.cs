e73e5e4d047f6f1d144615bf44018a715d851bc5
Vasilije Gavric <vasilije.gavric@gmail.com>
2022-05-08 15:38:41 +0200

    Initial check in.

---
+++ DUO2ChampionshipWebApi/Controllers/VersionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUO2ChampionshipWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<VersionController> _logger;
+        public IConfiguration _configuration { get; }
+        public VersionController(ILogger<VersionController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string GetVersion()
+        {
+            return this._configuration["Version"];
+        }
+        
+    }
+}
